63ab4cf22e1a3b2d3dc7cd3f1d7ac1d5ce16699a
cnwang <cnwang406@gmail.com>
2020-04-06 18:15:44 +0800

    no message

--- taiwan_history.py
+++ taiwan_history.py
@@ -5,12 +5,14 @@ import ssl
 import sys
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-
+from datetime import date,timedelta 
 import pandas as pd
 
 print(sys.version_info)
 
 url=r'https://corona.lmao.ninja/v2/historical'
+startDate=date(2020,2,27)
+endDate=date.today()+timedelta(days=5)
 
 http=PoolManager()
 r=http.request('GET',url)
@@ -33,16 +35,18 @@ df['date2']=pd.to_datetime(df['date'])
 fig,ax1 = plt.subplots()
 ax2=ax1.twinx()
 ax1.scatter(x=df['date2'],y=df['acc'],alpha=0.5,marker='o',color='blue')
+#ax1.scatter(x=df['date2'],y=df['']
 ax1.set_ylabel('new cases by day',color='blue')
 ax2.scatter(x=df['date2'],y=df['cases'],marker='.',color='red')
 ax2.set_ylabel('accumlate cases',color='red')
 ax1.set_xlabel('Date')
 
-print(type(df['date']))
-ax1.xaxis.set_major_locator(mdates.DayLocator(interval=14))
+ax1.xaxis.set_major_locator(mdates.DayLocator(interval=7))
 ax1.xaxis.set_minor_locator(mdates.DayLocator(interval=3))
 ax1.xaxis.grid(True,which='both')
 ax1.set_xticklabels(df['date2'],rotation=90)
 ax1.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
-plt.title(f'COV-19, Taiwan\ndata from {url}')
+ax1.set_xlim([startDate,endDate])
+fig.subplots_adjust(top=0.8)
+plt.title(f'COV-19, Taiwan\ndata from {url}\n{startDate} ~ {date.today()}')
 plt.show()
